6f0f4ef9e51420ab3c190313078eadbea4ccf60d
mac <mac@macnoMacBook-puro.local>
2018-01-03 18:24:13 +0900

    Hide firebase certificate

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,16 +1,43 @@
 import React from 'react';
 import { StyleSheet, View, Text, TextInput, TouchableHighlight } from 'react-native';
 
-class MemoLoginScreen extends React.Component {
+
+class LoginScreen extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  }
+  // eslint-disable-next-line
+  handleSubmit() {
+  // this.props.navigation.navigate('Home')
+
+  // Log in!!
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.title}>
           ログイン
         </Text>
-        <TextInput style={styles.input} value="Email Address" />
-        <TextInput style={styles.input} value="Password" />
-        <TouchableHighlight style={styles.button} onPress={() => {}}>
+        <TextInput
+          style={styles.input}
+          value={this.state.email}
+          onChangeText={(text) => { this.setState({ email: text }); }}
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Email Address"
+        />
+        <TextInput
+          style={styles.input}
+          value={this.state.password}
+          onChangeText={(text) => { this.setState({ password: text }); }}
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Email Address"
+          secureTextEntry
+        />
+        <TouchableHighlight style={styles.button} onPress={this.handleSubmit.bind(this)}>
           <Text style={styles.bottomTitle}>ログインする</Text>
         </TouchableHighlight>
       </View>
@@ -54,4 +81,4 @@ const styles = StyleSheet.create({
 
 });
 
-export default MemoLoginScreen;
+export default LoginScreen;
